35255ff4b45bbc683af7d578ba09fb3497eae0fd
Ritik Raj <ritik2903raj@gmail.com>
2021-09-18 23:45:11 +0530

    scheduling js task

---
+++ javascript.js
@@ -0,0 +1,23 @@
+
+// if we click on the start/reset
+//if we r playing
+//reload page
+// if we r not playing
+// set score to 0
+// show count down box
+// reduse time by 1 sec in loop
+// time left?
+// yes->continue
+// no->gameover
+ // change button to reset
+ // generate new q&a
+
+ // if we click on answer box
+ // if we r playing
+ // correct?
+ //yes
+ //increase score
+ // show correct box for 1 sec
+ // genearte new q & a
+ // no
+ // show try again box for 1 sec
